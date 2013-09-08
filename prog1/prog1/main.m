6c9f185d887f21844eae6b8548592e6ea40a8ce7
Josh Hess <jhess@Joshs-MacBook-Air.local>
2013-09-07 23:54:45 -0700

    So Tired.

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -8,6 +8,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
 
         NSLog(@"Programming is fun!");
+        NSLog(@"And programming in Objective-C is even more fun!!!");
         
     }
     return 0;
