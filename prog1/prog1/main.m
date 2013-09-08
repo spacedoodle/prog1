c36e92a6e63efb6ad0ab21ec5cbc8cd05bf08bdc
Josh Hess <jhess@Joshs-MacBook-Air.local>
2013-09-07 23:26:57 -0700

    Initial Xcode commit test

---
+++ prog1/prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Josh Hess on 9/7/13.
+//  Copyright (c) 2013 Josh Hess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
